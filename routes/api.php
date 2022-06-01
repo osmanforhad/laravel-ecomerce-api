dda593aa5c8d15f3640977633b33e1ce06f7eb0f
osmanforhad <developer.osmanforhad@gmail.com>
2022-06-01 12:08:03 +0600

    authentication check setup

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,10 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
+
+    Route::get('/checkingAuthenticated', function() {
+        return response()->json(['message'=>'You are in', 'status=>200'], 200);
+    });
+
     Route::post('logout', [AuthController::class, 'logout']);
 });
